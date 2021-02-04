6ef4e49b2d025cef1806edde0f8d1ecffe6133a1
hlchen02 <hlchen02@wisedu.com>
2021-02-04 10:31:15 +0800

    整合mybatis逆向工程 和 功能测试Junit

---
+++ coupon-app/src/main/java/com/chl/couponapp/service/CouponService.java
@@ -0,0 +1,11 @@
+package com.chl.couponapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CouponService {
+
+    public void print(){
+        System.out.println("first service");
+    }
+}
--- coupon-app/src/test/java/com/chl/couponapp/CouponAppApplicationTests.java
+++ coupon-app/src/test/java/com/chl/couponapp/CouponAppApplicationTests.java
@@ -1,13 +1,24 @@
 package com.chl.couponapp;
 
+import com.chl.couponapp.service.CouponService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CouponAppApplication.class)
 class CouponAppApplicationTests {
+
+    @Resource
+    private CouponService couponService;
 
     @Test
     void contextLoads() {
+        couponService.print();
+        System.out.println("first Test method");
     }
 
 }
