02be69221c1cf335e8619483a006b115feee6edc
hlchen02 <hlchen02@wisedu.com>
2021-05-11 17:47:48 +0800

    Revert "JMH基准测试"=

--- coupon-app/src/main/java/com/chl/couponapp/service/CouponService.java
+++ coupon-app/src/main/java/com/chl/couponapp/service/CouponService.java
@@ -1,17 +1,8 @@
 package com.chl.couponapp.service;
 
-import com.chl.couponapp.constant.Constant;
-import com.chl.couponapp.domain.TCoupon;
-import com.chl.couponapp.domain.TCouponExample;
-import com.chl.couponapp.mapper.TCouponMapper;
 import com.chl.userserviceapi.service.IUserService;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.Date;
-import java.util.List;
 
 /**
  * @author admin
@@ -19,26 +10,12 @@ import java.util.List;
 @Service
 public class CouponService {
 
-    //@Reference
-    //private IUserService iUserService;
+    @Reference
+    private IUserService iUserService;
 
-    @Resource
-    private TCouponMapper tCouponMapper;
-
-    /***
-     * 获取有效时间的可用优惠券列表
-     * @return
-     */
-    public List<TCoupon> getCouponList(){
-        TCouponExample example = new TCouponExample();
-        example.createCriteria().andStatusEqualTo(Constant.USERFUL)
-                .andStartTimeLessThan(new Date()).andEndTimeGreaterThan(new Date());
-        return tCouponMapper.selectByExample(example);
+    public String getUserById(int id){
+        return iUserService.getUserById(id).toString();
     }
-
-    //public String getUserById(int id){
-    //    return iUserService.getUserById(id).toString();
-    //}
 
     public void print(){
         System.out.println("first service");
