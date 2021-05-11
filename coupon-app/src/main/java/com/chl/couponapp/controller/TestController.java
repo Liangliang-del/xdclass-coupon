a854d6b9dce1b788d1826f977c0401eb07e1191f
hlchen02 <hlchen02@wisedu.com>
2021-05-11 18:05:45 +0800

    commit

--- coupon-app/src/main/java/com/chl/couponapp/controller/TestController.java
+++ coupon-app/src/main/java/com/chl/couponapp/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.chl.couponapp.controller;
 
+import com.chl.couponapp.domain.TCoupon;
 import com.chl.couponapp.service.CouponService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -17,9 +19,14 @@ public class TestController {
         return "test";
     }
 
-    @RequestMapping("/getUser")
-    public String getUser(){
-        return couponService.getUserById(5);
+    //@RequestMapping("/getUser")
+    //public String getUser(){
+    //    return couponService.getUserById(5);
+    //}
+
+    @RequestMapping("/getCouponList")
+    public List<TCoupon> getCouponList(){
+        return couponService.getCouponList();
     }
 
 }
