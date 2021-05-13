6e88d1b73748b41a8183b5e4048ea3e6ef3358fa
hlchen02 <hlchen02@wisedu.com>
2021-05-13 15:33:00 +0800

    commit

--- coupon-app/src/main/java/com/chl/couponapp/service/schedule/UpdateCouponJob.java
+++ coupon-app/src/main/java/com/chl/couponapp/service/schedule/UpdateCouponJob.java
@@ -1,6 +1,8 @@
 package com.chl.couponapp.service.schedule;
 
 import com.chl.couponapp.service.CouponService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,14 @@ import javax.annotation.Resource;
 @Service
 public class UpdateCouponJob {
 
+    private static final Logger logger = LoggerFactory.getLogger(UpdateCouponJob.class);
+
     @Resource
     private CouponService couponService;
 
     @Scheduled(cron = "0/10 * * * * ?")
     public void updateCoupon(){
-        System.out.println("enter updateCoupon Job");
+        logger.info("enter updateCoupon Job");
         couponService.updateCouponMap();
     }
 }
