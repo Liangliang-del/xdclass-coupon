6e632807ba3452cf2e7ee77c9320b33bcdc25319
hlchen02 <hlchen02@wisedu.com>
2021-05-13 11:22:16 +0800

    commit

--- coupon-app/src/test/java/com/chl/couponapp/JMHSpringBootTest.java
+++ coupon-app/src/test/java/com/chl/couponapp/JMHSpringBootTest.java
@@ -15,30 +15,30 @@ public class JMHSpringBootTest {
 
     //private CouponService couponService;
 
-    public static void main(String[] args) throws RunnerException {
-        Options options = new OptionsBuilder().include(JMHSpringBootTest.class.getName()+".*")
-                .warmupIterations(2)
-                .measurementIterations(2)
-                .forks(1).build();
-        new Runner(options).run();
-    }
-
-    /**
-     * setup初始化容器的时候只执行一次
-     */
-    @Setup(Level.Trial)
-    public void init(){
-        String arg = "";
-        ConfigurableApplicationContext context = SpringApplication.run(CouponAppApplication.class, arg);
-        //couponService = context.getBean(CouponService.class);
-    }
-
-
-    /**
-     * benchmark执行多次，此注解代表触发我们所要进行基准测试的方法
-     */
-    @Benchmark
-    public void test(){
-        //System.out.println(couponService.getCouponList());
-    }
+    //public static void main(String[] args) throws RunnerException {
+    //    Options options = new OptionsBuilder().include(JMHSpringBootTest.class.getName()+".*")
+    //            .warmupIterations(2)
+    //            .measurementIterations(2)
+    //            .forks(1).build();
+    //    new Runner(options).run();
+    //}
+    //
+    ///**
+    // * setup初始化容器的时候只执行一次
+    // */
+    //@Setup(Level.Trial)
+    //public void init(){
+    //    String arg = "";
+    //    ConfigurableApplicationContext context = SpringApplication.run(CouponAppApplication.class, arg);
+    //    //couponService = context.getBean(CouponService.class);
+    //}
+    //
+    //
+    ///**
+    // * benchmark执行多次，此注解代表触发我们所要进行基准测试的方法
+    // */
+    //@Benchmark
+    //public void test(){
+    //    //System.out.println(couponService.getCouponList());
+    //}
 }
