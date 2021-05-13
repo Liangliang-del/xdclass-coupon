4ce7fd232f8a7a31785d7963b4ce3d0f5dc7e976
hlchen02 <hlchen02@wisedu.com>
2021-05-13 13:52:09 +0800

    commit

--- coupon-app/src/main/java/com/chl/couponapp/CouponAppApplication.java
+++ coupon-app/src/main/java/com/chl/couponapp/CouponAppApplication.java
@@ -5,11 +5,13 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.chl.couponapp.mapper")
 @EnableDubboConfig
 @DubboComponentScan("com.chl.userapp.service.dubbo")
+@EnableScheduling
 public class CouponAppApplication {
 
     public static void main(String[] args) {
