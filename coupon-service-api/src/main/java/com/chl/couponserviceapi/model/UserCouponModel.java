547531a215ef601c7deae6ff8d6e5f59d12c33e1
hlchen02 <hlchen02@wisedu.com>
2021-05-18 10:10:58 +0800

    commit

---
+++ coupon-service-api/src/main/java/com/chl/couponserviceapi/model/UserCouponModel.java
@@ -0,0 +1,36 @@
+package com.chl.couponserviceapi.model;
+
+/**
+ * @author admin
+ */
+public class UserCouponModel {
+    private Integer couponId;
+
+    private Integer userId;
+
+    private Integer orderId;
+
+    public Integer getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(Integer couponId) {
+        this.couponId = couponId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+}
